1037e3ca21b4b17f1db28239ab61b63e4a18ba16
Yauhen Charniausky <89525528+YauhenC@users.noreply.github.com>
2021-09-20 13:36:27 +0300

    Update lambda.tf

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -3,7 +3,7 @@ resource "aws_lambda_function" "this" {
 
   filename = var.filename
   handler  = "io.micronaut.function.aws.proxy.MicronautLambdaHandler"
-  runtime  = "java 11"
+  runtime  = "java11"
 
   role = aws_iam_role.lambda_role.arn
 }
