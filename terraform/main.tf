54db99b9cf73f5fe7b0e53a3c86c38f6e1953921
Yauhen Charniausky <89525528+YauhenC@users.noreply.github.com>
2021-09-21 13:16:47 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 terraform {
 
   backend "s3" {
-    bucket = "devops-challenge-terraform"
+    bucket = "devops-challenge-terraform0"
     key    = "lambda-challenge/terraform.tfstate"
     region = "us-east-2"
   }
