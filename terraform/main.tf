25862dd0a5079b2b7d4c4213c30a432c8f4efb7f
Yauhen Charniauski <Yauhen.Charniausky@itechart-group.com>
2021-09-27 17:59:02 +0300

    fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,9 @@
 terraform {
 
   backend "s3" {
-    bucket = "devops-challenge-tfstate"
+    bucket = "nm-devops-challenge-tfstate"
     key    = "lambda-challenge/terraform.tfstate"
-    region = "us-east-1"
+    region = "eu-central-1"
   }
 
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,9 +13,9 @@ variable "runtime" {
 variable "handler" {
   description = "Specify entrypoint function for Lambda"
 }
-variable "memory_size" {
-  description = "Specify Lambda memory limit. Default is 128 M"
-}
 variable "route_key" {
   description = "Specify route path for AWS API Gateway"
+}
+variable "memory_size" {
+  description = "Specify Lambda memory limit. Default is 128 Mb"
 }
